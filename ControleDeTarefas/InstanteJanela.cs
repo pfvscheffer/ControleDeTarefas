83223ed64e0e2c682028afae26f560d9d60a257e
pfvscheffer <pfvscheffer@users.noreply.github.com>
2017-11-22 03:56:08 -0200

    Corrigido erro que campos internal não são mostrados no DataGridView

--- ControleDeTarefas/InstanteJanela.cs
+++ ControleDeTarefas/InstanteJanela.cs
@@ -16,20 +16,19 @@ namespace ControleDeTarefas
         private Janela parent;
 
         internal string Nome { get { return nome; } }
-        internal DateTime Inicio { get { return inicio; } }
-        internal DateTime? Fim { get { return fim; } }
+        public DateTime Inicio { get { return inicio; } }
+        public DateTime? Fim { get { return fim; } }
         internal TimeSpan Duracao { get { return duracao ?? TimeSpan.Zero; } }
         internal Janela Parent { get { return parent; } }
         internal IntPtr Handle { get { return parent.Handle; } }
-        internal int ProcID { get { return parent.Processo.ProcId; } }
-        internal string ProcNome { get { return parent.Processo.Nome; } }
+        public int ProcID { get { return parent.Processo.ProcId; } }
+        public string ProcNome { get { return parent.Processo.Nome; } }
 
         internal InstanteJanela(Janela parent) : this(ObterNome(parent.Handle), parent, DateTime.Now)
         {
             this.inicio = DateTime.Now;
             this.nome = ObterNome(parent.Handle);
             this.parent = parent;
-
         }
 
         internal InstanteJanela(string nome, Janela parent, DateTime inicio)
